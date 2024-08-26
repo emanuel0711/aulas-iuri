0331bb2e06aec877a505ace378bb680dfabe0cf4
emanuel0711 <emanuelchaves321@gmail.com>
2024-08-26 19:34:22 -0300

    Update ponto2d.ts

--- perguntas.txt/ponto2d.ts
+++ perguntas.txt/ponto2d.ts
@@ -1,29 +1,70 @@
-import { Ponto2D } from './ponto2d';
-export class Ponto2D {
-    private _x: number;
-    private _y: number;
 
-    // Construtor que pode ser chamado de duas formas
-    constructor(x: number = 0, y: number = 0) {
-        this._x = x;
-        this._y = y;
+export class Ponto2DBase{
+    x: number
+    y: number
+
+    constructor(){
+        this.x = 0
+        this.y = 0
     }
 
-    // Getter e Setter para x
-    get x(): number {
-        return this._x;
+    setX(valorx: number){
+        this.x = valorx
+    }
+    setY(valory: number){
+        this.y = valory
+    }
+    getX(): number{
+        return this.x
+    }
+    getY():number{
+        return this.y
     }
 
-    set x(value: number) {
-        this._x = value;
+    toString(): string{
+        return `
+        X: ${this.x}
+        Y: ${this.y}
+        `
+    }
+    
+    equals(outroPonto: Ponto2DBase): boolean{
+        return this.x === outroPonto.getX() && this.y === outroPonto.getY()
     }
 
-    // Getter e Setter para y
-    get y(): number {
-        return this._y;
+    /*
+     distancia = raizV(x2 - x1) *2 + (y2 - y1)*2
+    */
+    distancia(outroPonto: Ponto2DBase):number {
+        let dx = outroPonto.getX() - this.x
+        let dy = outroPonto.getY() - this.y
+
+
+        //serve para fazer raiz quadrada
+        return Math.sqrt((dx * dx) + (dy * dy))
     }
 
-    set y(value: number) {
-        this._y = value;
+    clone(): Ponto2DBase{
+        return new Ponto2DBase()
     }
 }
+
+export class Ponto2DPadrao extends Ponto2DBase{
+    constructor(x: number, y: number){
+        super()
+        this.setX(x)
+        this.setY(y)
+    }
+}
+
+export class Ponto22 extends Ponto2DBase{
+    constructor(outroPonto: Ponto2DBase){
+        super()
+        this.setX(outroPonto.getX())
+        this.setY(outroPonto.getY())
+    }
+}
+
+let ponto1 = new Ponto2DBase()
+let ponto2 = new Ponto2DPadrao(10, 10)
+let ponto3 = new Ponto22(ponto2)
