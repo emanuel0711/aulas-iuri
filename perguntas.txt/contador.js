43df429d18fd81d4bd3048b0f143fee9013fcfa4
emanuel0711 <emanuelchaves321@gmail.com>
2024-08-26 19:31:14 -0300

    Update contador.js
    é

--- perguntas.txt/contador.js
+++ perguntas.txt/contador.js
@@ -1,48 +1,20 @@
-export function runTests(){
-    console.log('Testando Contador...')
-}
-
-class Contador {
-    private _valor: number;
-    
-         constructor() {
-            this._valor = 0;
-        }
-    
-        valor() {
-            return this._valor;
-        }
-    
-        incrementar() {
-            this._valor++;
-        }
-    }
-    
-    const contador = new Contador();
-    
-    // Verifica se o valor inicial é 0
-    console.assert(contador.valor() === 0, "Teste falhou: valor inicial não é 0");
-    
-    // Incrementa o valor e verifica se ele foi incrementado corretamente
-    contador.incrementar();
-    console.assert(contador.valor() === 1, "Teste falhou: valor não foi incrementado corretamente");
-    
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Contador = void 0;
 var Contador = /** @class */ (function () {
     function Contador() {
-        this.valor = 0;
-        // Inicializa o contador com o valor 0
+        this.contador = 0;
     }
     Contador.prototype.zerar = function () {
-        this.valor = 0;
+        this.contador = 0;
     };
-    Contador.prototype.incrementar = function () {
-        this.valor++;
+    //++ (é igual a += 1) tbm tem o -- (é igual -= 1)
+    Contador.prototype.incrementar = function (number) {
+        this.contador += number;
     };
-    Contador.prototype.Valor = function () {
-        return this.valor;
+    Contador.prototype.valorAtual = function () {
+        return this.contador;
     };
     return Contador;
 }());
- 
-console.log()
+exports.Contador = Contador;
